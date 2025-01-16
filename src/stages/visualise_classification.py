c2fba50188d061486e72b0dc10e4159089e7a41a
antngh <antonhawthorne2@gmail.com>
2025-01-16 17:03:52 +0000

    chore/add_classification_script updated script to fix issues and added logistic regression. Handle train and test splitting now

--- src/stages/visualise_classification.py
+++ src/stages/visualise_classification.py
@@ -25,6 +25,8 @@ def calculate_correctness_direction(activations, label_df):
     # Basically the centroid over the data + the correctness direction (or it flipped) should take you to the centroid of the class.
     mu = activations.mean(axis=0)
 
+    label_df = label_df.reset_index(drop=True)
+
     s = pt.ones(activations.shape[0])
     s[~label_df["correct"]] = (
         -1
@@ -33,7 +35,8 @@ def calculate_correctness_direction(activations, label_df):
     correctness_direction = pt.mean((activations - mu) * s[:, None], dim=0)
     return (
         correctness_direction,
-        (activations - mu) @ correctness_direction,
+        # (activations - mu) @ correctness_direction,
+        mu,
     )  # From algebra should be /s, but x/1=x*1 and x/-1=x*-1 so can just multiply by s
 
 
@@ -45,11 +48,21 @@ def evaluate_classification(labels, correctness, experiment_path, extra_info=Non
     optimal_idx = np.argmax(youden_index)
     optimal_threshold = thresholds[optimal_idx]
 
-    acc = accuracy_score(df["label"], df["correctness"] > optimal_threshold)
+    acc = accuracy_score(df["label"], df["correctness"] >= optimal_threshold)
     roc_auc = auc(fpr, tpr)
 
-    ax = sns.histplot(df[~df["label"]]["correctness"], label="Incorrect")
-    ax = sns.histplot(df[df["label"]]["correctness"], label="Correct", ax=ax)
+    # ax = sns.histplot(
+    #     df[~df["label"]]["correctness"],
+    #     label="Incorrect",
+    #     bins=20,
+    # )
+    # ax = sns.histplot(df[df["label"]]["correctness"], label="Correct", bins=20, ax=ax)
+    ax = sns.histplot(
+        data=df,
+        x="correctness",
+        hue="label",
+        bins=100,
+    )
     ax.axvline(
         optimal_threshold,
         color="red",
@@ -61,9 +74,11 @@ def evaluate_classification(labels, correctness, experiment_path, extra_info=Non
     plt.savefig(
         experiment_path / "classifier_separation.png", dpi=300, bbox_inches="tight"
     )
+    plt.clf()
 
     plt.figure(figsize=(8, 6))
     plt.plot(fpr, tpr, label=f"ROC curve (AUC = {roc_auc:.4f})")
+
     plt.scatter(
         fpr[optimal_idx],
         tpr[optimal_idx],
@@ -77,18 +92,17 @@ def evaluate_classification(labels, correctness, experiment_path, extra_info=Non
     plt.legend(loc="lower right")
     plt.tight_layout()
     plt.savefig(experiment_path / "classifier_roc.png", dpi=300, bbox_inches="tight")
-
-    experiment_path.write
+    plt.clf()
 
     logging_config = {
         **{
-            "accuracy": acc,
-            "roc_auc": roc_auc,
-            "optimal_threshold": optimal_threshold,
+            "accuracy": float(acc),
+            "roc_auc": float(roc_auc),
+            "optimal_threshold": float(optimal_threshold),
         },
         **(extra_info or {}),
     }
-    with experiment_path.open("w", encoding="utf-8") as file:
+    with (experiment_path / "info.json").open("w", encoding="utf-8") as file:
         json.dump(logging_config, file, indent=4, ensure_ascii=False)
 
 
@@ -103,9 +117,6 @@ def load_labels_df_and_activations(
     labels_df = pd.read_csv(question_answer_file)
 
     if first_batch is not None:
-        if first_batch % batch_size != 0:
-            raise ValueError("first_batch must be a multiple of batch_size")
-
         labels_df = labels_df.iloc[
             first_batch
             * batch_size : ((first_batch + n_batches) * batch_size if n_batches else -1)
@@ -115,26 +126,30 @@ def load_labels_df_and_activations(
 
     # todo assumes that the last number in the file name is the index of the first entry in the batch, and that they're batched in 25 entries
     tensors = []
+    any_for_layer = False
     for file in sorted(
         Path(activations_dir).iterdir(),
         key=lambda file_: _act_file_to_batch_idx(file_),
     ):
         if f"layer_{layer}" not in str(file):
             continue
+        any_for_layer = True
 
         batch_first_entry_index = _act_file_to_batch_idx(file)
         if first_batch is not None:
-            if first_batch > batch_first_entry_index:
+            if first_batch * 25 > batch_first_entry_index + 25 - 1:
                 continue
             if (
                 n_batches is not None
-                and batch_first_entry_index > first_batch + n_batches
+                and batch_first_entry_index > (first_batch + n_batches - 1) * 25
             ):
                 continue
 
         act_tensor = pt.load(file)
         tensors.append(act_tensor)
 
+    if not any_for_layer:
+        raise FileNotFoundError(f"No activations found for layer {layer}")
     activations = pt.cat(tensors, axis=0).to("cpu")
 
     if len(labels_df) != activations.shape[0]:
@@ -142,30 +157,45 @@ def load_labels_df_and_activations(
             f"Number of labels ({len(labels_df)}) does not match number of activations ({activations.shape[0]})"
         )
 
-    return labels_df, activations
+    return labels_df.reset_index(drop=True), activations
 
 
 def evaluate_correctness_direction_and_classifier(
-    activations, labels_df, experiment_path
+    X_train,
+    X_test,
+    train_label_df,
+    test_label_df,
+    experiment_path,
+    # train_frac=0.8,
 ):
-    pca_pipeline = make_pipeline(StandardScaler(), PCA(n_components=2))
-    pca_activations = pca_pipeline.fit_transform(activations)
+    # train_indices = np.random.uniform(0, 1, len(labels_df)) < train_frac
+    # test_indices = ~train_indices
 
-    correctness_direction, labels_df["correctness"] = calculate_correctness_direction(
-        activations, labels_df
-    )
+    # train_label_df = labels_df.iloc[train_indices]
+    # test_label_df = labels_df.iloc[test_indices]
+
+    pca_pipeline = make_pipeline(StandardScaler(), PCA(n_components=2))
+    _ = pca_pipeline.fit_transform(X_train)
+    pca_activations_test = pca_pipeline.transform(X_test)
+
+    correctness_direction, mu = calculate_correctness_direction(X_train, train_label_df)
+    test_label_df["correctness"] = (X_test - mu) @ correctness_direction
+
+    # labels_df["correctness"]
 
     correctness_end_pca = pca_pipeline.transform(
         correctness_direction[None, :]
     ).squeeze()
 
-    zero_pca = pca_pipeline.transform([[0] * activations.shape[-1]]).squeeze()
+    zero_pca = pca_pipeline.transform([[0] * X_train.shape[-1]]).squeeze()
     correctness_direction_pca = (
         correctness_end_pca - zero_pca
     )  # need to transform the begining and end of the vector so that we transform the vector so that we can get the difference in pca space.
 
     ax = sns.scatterplot(
-        x=pca_activations[:, 0], y=pca_activations[:, 1], hue=labels_df["correct"]
+        x=pca_activations_test[:, 0],
+        y=pca_activations_test[:, 1],
+        hue=test_label_df["correct"],
     )
     ax.quiver(
         0,
@@ -181,78 +211,123 @@ def evaluate_correctness_direction_and_classifier(
     ax.legend()
     plt.tight_layout()
     plt.savefig(experiment_path / "pca.png", dpi=300, bbox_inches="tight")
+    plt.clf()
 
-    evaluate_classification(
-        labels_df["correct"],
-        labels_df["correctness"],
-        experiment_path / "correctness_direction",
-    )
-
-
-def train_and_evaluate_mlp_classifier(
-    labels_df,
-    activations,
-    experiment_path,
-    verbose=False,
-    train_frac=0.8,
-    n_epochs=100,
-    lr=0.01,
-):
-    train_indices = np.random.uniform(0, 1, len(labels_df)) < train_frac
-    test_indices = ~train_indices
-
-    train_label_df = labels_df.iloc[train_indices]
-    test_label_df = labels_df.iloc[test_indices]
-
-    class SingleLayerPerceptron(nn.Module):
-        def __init__(self, input_size, output_size):
-            super(SingleLayerPerceptron, self).__init__()
-            self.fc = nn.Linear(input_size, 128)
-            self.fc2 = nn.Linear(128, output_size)
-
-        def forward(self, x):
-            return self.fc2(nn.functional.relu(self.fc(x)))
-
-    model = SingleLayerPerceptron(activations.shape[-1], 1)
-
-    criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.SGD(model.parameters(), lr=lr)
-
-    for epoch in range(n_epochs):
-        optimizer.zero_grad()
-        outputs = model(activations[train_indices])
-
-        # Convert targets to float tensor
-        targets = pt.tensor(train_label_df["correct"].to_numpy(), dtype=pt.float32)
-
-        # Ensure outputs match target shape
-        outputs = outputs.squeeze()
-
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
-
-        if verbose and epoch % 10 == 0:
-            print(f"Epoch [{epoch+1}/100], Loss: {loss.item():.4f}")
-
-    test_label_df["mlp_classifier_pred"] = (
-        pt.nn.functional.sigmoid(model(activations[test_indices]))
-        .detach()
-        .numpy()
-        .squeeze()
-    )
-
+    correctness_direction_path = experiment_path / "correctness_direction"
+    correctness_direction_path.mkdir(parents=True, exist_ok=True)
     evaluate_classification(
         test_label_df["correct"],
-        test_label_df["mlp_classifier_pred"],
-        experiment_path=experiment_path / "mlp_classifier",
-        extra_info={
-            "model_architecture": str(model),
-            "train_frac": train_frac,
-            "n_epochs": n_epochs,
-            "lr": lr,
-        },  # Note that this isn't intended for logging variation to this classifier, just for logging the parameters used.
+        test_label_df["correctness"],
+        correctness_direction_path,
     )
+
+
+def evaluate_logistic_regression_classifier(
+    # activations,
+    X_train,
+    X_test,
+    train_label_df,
+    test_label_df,
+    experiment_path,
+    # train_frac=0.8,
+):
+    # train_indices = np.random.uniform(0, 1, len(labels_df)) < train_frac
+    # test_indices = ~train_indices
+
+    # train_label_df = labels_df.iloc[train_indices]
+    # test_label_df = labels_df.iloc[test_indices]
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    from sklearn.linear_model import LogisticRegression
+
+    model = LogisticRegression(random_state=42, solver="lbfgs", max_iter=1000)
+    model.fit(X_train, train_label_df["correct"])
+
+    # Step 4: Make predictions
+    test_label_df["correctness"] = model.predict(X_test)
+
+    correctness_direction_path = experiment_path / "logistic_regression_classifier"
+    correctness_direction_path.mkdir(parents=True, exist_ok=True)
+    evaluate_classification(
+        test_label_df["correct"],
+        test_label_df["correctness"],
+        correctness_direction_path,
+        extra_info={
+            "model": str(model),
+        },
+    )
+
+
+# def train_and_evaluate_mlp_classifier(
+#     labels_df,
+#     activations,
+#     experiment_path,
+#     verbose=False,
+#     train_frac=0.8,
+#     n_epochs=100,
+#     lr=0.01,
+# ):
+#     train_indices = np.random.uniform(0, 1, len(labels_df)) < train_frac
+#     test_indices = ~train_indices
+
+#     train_label_df = labels_df.iloc[train_indices]
+#     test_label_df = labels_df.iloc[test_indices]
+
+#     class SingleLayerPerceptron(nn.Module):
+#         def __init__(self, input_size, output_size):
+#             super(SingleLayerPerceptron, self).__init__()
+#             self.fc = nn.Linear(input_size, 128)
+#             self.fc2 = nn.Linear(128, output_size)
+
+#         def forward(self, x):
+#             return self.fc2(nn.functional.relu(self.fc(x)))
+
+#     model = SingleLayerPerceptron(activations.shape[-1], 1).to(pt.float32)
+
+#     criterion = nn.BCEWithLogitsLoss()
+#     optimizer = optim.SGD(model.parameters(), lr=lr)
+
+#     for epoch in range(n_epochs):
+#         optimizer.zero_grad()
+#         # print(f"{model.dtype=}")
+#         outputs = model(activations[train_indices].to(pt.float32))
+
+#         # Convert targets to float tensor
+#         targets = pt.tensor(train_label_df["correct"].to_numpy(), dtype=pt.float32)
+
+#         # Ensure outputs match target shape
+#         outputs = outputs.squeeze()
+
+#         loss = criterion(outputs, targets)
+#         loss.backward()
+#         optimizer.step()
+
+#         if verbose and epoch % 10 == 0:
+#             print(f"Epoch [{epoch+1}/100], Loss: {loss.item():.4f}")
+
+#     test_label_df["mlp_classifier_pred"] = (
+#         pt.nn.functional.sigmoid(model(activations[test_indices].to(pt.float32)))
+#         .detach()
+#         .numpy()
+#         .squeeze()
+#     )
+
+#     mlp_classification_path = experiment_path / "mlp_classifier"
+#     mlp_classification_path.mkdir(parents=True, exist_ok=True)
+#     evaluate_classification(
+#         test_label_df["correct"],
+#         test_label_df["mlp_classifier_pred"],
+#         experiment_path=mlp_classification_path,
+#         extra_info={
+#             "model_architecture": str(model),
+#             "train_frac": train_frac,
+#             "n_epochs": n_epochs,
+#             "lr": lr,
+#         },  # Note that this isn't intended for logging variation to this classifier, just for logging the parameters used.
+#     )
 
 
 def get_experiment_path(output_path, _experiment_id=None):
@@ -286,7 +361,9 @@ def classifier_experiment_run(
     n_batches: str | None = None,
     batch_size: int = 25,
     check_correctness_classifier: bool = True,
-    check_mlp_classifier: bool = True,
+    check_logistic_regression_classifier: bool = True,
+    # check_mlp_classifier: bool = True,
+    train_frac=0.8,
     output_path: str = "./experiment_results",
     notes: str = "",
     _experiment_id: int = None,  # careful, only use this to overwrite existing experiments
@@ -301,9 +378,9 @@ def classifier_experiment_run(
         n_batches=n_batches,
         batch_size=batch_size,
     )
-    if not check_correctness_classifier and not check_mlp_classifier:
+    if not check_correctness_classifier and not check_logistic_regression_classifier:
         raise ValueError(
-            "At least one of check_correctness_classifier or check_mlp_classifier must be True"
+            "At least one of check_correctness_classifier or check_logistic_regression_classifier must be True"
         )
 
     experiment_path = get_experiment_path(output_path, _experiment_id=_experiment_id)
@@ -317,41 +394,60 @@ def classifier_experiment_run(
         "n_batches": n_batches,
         "batch_size": batch_size,
         "check_correctness_classifier": check_correctness_classifier,
-        "check_mlp_classifier": check_mlp_classifier,
+        "check_logistic_regression_classifier": check_logistic_regression_classifier,
+        "train_frac": train_frac,
         "timestamp": str(datetime.now()),
         "notes": notes,
     }
-    with experiment_path.open("w", encoding="utf-8") as file:
+    with (experiment_path / "info.json").open("w", encoding="utf-8") as file:
         json.dump(logging_config, file, indent=4, ensure_ascii=False)
+
+    train_indices = np.random.uniform(0, 1, len(labels_df)) < train_frac
+    test_indices = ~train_indices
+    train_label_df = labels_df.iloc[train_indices]
+    test_label_df = labels_df.iloc[test_indices]
+    X_train = activations[train_indices]
+    X_test = activations[test_indices]
 
     if check_correctness_classifier:
         evaluate_correctness_direction_and_classifier(
-            activations, labels_df, experiment_path
+            X_train, X_test, train_label_df, test_label_df, experiment_path
         )
 
-    if check_mlp_classifier:
-        train_and_evaluate_mlp_classifier(labels_df, activations, experiment_path)
+    # if check_mlp_classifier:
+    #     train_and_evaluate_mlp_classifier(labels_df, activations, experiment_path)
+    if check_logistic_regression_classifier:
+        evaluate_logistic_regression_classifier(
+            X_train, X_test, train_label_df, test_label_df, experiment_path
+        )
 
 
 if __name__ == "__main__":
     print("Run this from the command line at the base level of the repo")
+
     args_parser = argparse.ArgumentParser()
     # args_parser.add_argument("--config", dest="config", required=True) # is this needed?
     args_parser.add_argument("--model", dest="model", required=True)
     args_parser.add_argument("--layer", dest="layer", required=True)
-    args_parser.add_argument("--activations_dir", dest="head", required=True)
+    args_parser.add_argument("--activations-dir", dest="activations_dir", required=True)
     args_parser.add_argument(
         "--question-answer-file", dest="question_answer_file", required=True
     )
     args_parser.add_argument("--first-batch", dest="first_batch", default=None)
     args_parser.add_argument("--n-batches", dest="n_batches", default=None)
     args_parser.add_argument("--notes", dest="notes", default=None)
+    args_parser.add_argument("--random-seed", dest="random_seed", default=42)
     args = args_parser.parse_args()
+
+    np.random.seed(args.random_seed)
+
     classifier_experiment_run(
         model=args.model,
+        layer=args.layer,
         activations_dir=args.activations_dir,
         question_answer_file=args.question_answer_file,
-        first_batch=args.first_batch,
-        n_batches=args.n_batches,
+        first_batch=int(args.first_batch) if args.first_batch is not None else None,
+        n_batches=int(args.n_batches) if args.first_batch is not None else None,
         notes=args.notes,
     )
+    print("\n\n\nFinished.")
